e65c810fe2aefb29deed8975c8fa1894e8a82a94
verbospatial <veripax@outlook.com>
2026-08-01 13:25:26 +0000

    Update secondary node endpoint

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -3,7 +3,7 @@ export const GENESIS_BLOCK_ID =
   '00000000e29a7850088d660489b7b9ae2da763bc3bd83324ecc54eee04840adb';
 export const PRIMARY_CRUZBIT_NODE = `sure-formerly-filly.ngrok-free.app/${GENESIS_BLOCK_ID}`;
 export const SECONDARY_CRUZBIT_NODE =
-  'ungallant-unimpeding-kade.ngrok-free.dev/000000b179a6172473845cbc913598edef179aabb31108324694ca1b12a19e32';
+  'ungallant-unimpeding-kade.ngrok-free.dev/00000018133f9ecb5ff8779e659e6de64eb93e67a4eeed4a2d111d439db7f596';
 export const DEFAULT_CRUZBIT_NODE = PRIMARY_CRUZBIT_NODE;
 export const CRUZBIT_NODES = [
   { label: 'Primary', value: PRIMARY_CRUZBIT_NODE },
